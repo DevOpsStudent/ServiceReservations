a451e7eb53cc1daa8211fa4fa53a20c8492c10ad
ProDataMan <ProDataMan@outlook.com>
2018-02-25 23:01:32 -0600

    added multi push pop test

--- pdm/src/test/java/pdm/StackTest.java
+++ pdm/src/test/java/pdm/StackTest.java
@@ -39,4 +39,14 @@ public class StackTest {
 		Boolean result = stack.isEmpty();
 		assertFalse("Should be false",result);
 	}
+	@Test
+	public void multiPushPopTest() {
+		Stack stack = new Stack();
+		String expected = "Hello, World!";
+		String expected2 = "Good Bye Cruel World!";
+		stack.push(expected);
+		stack.push(expected2);
+		String actual = stack.pop();
+		assertEquals("Should be " + expected2, expected2, actual);
+	}
 }
